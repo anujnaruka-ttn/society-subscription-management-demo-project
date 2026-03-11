3d525076c1b6d4364ab2eb7630bbe3bd4bdd7328
anujnaruke-ttn <anuj.naruka@tothenew.com>
2026-03-11 12:36:32 +0530

    Added types to req and res

---
+++ src/controllers/AUTH.ts
@@ -0,0 +1,30 @@
+import { Request, Response } from "express";
+import { catchAsync } from "../utils/catchAsync";
+
+const adminLogin = catchAsync(
+    async (req: Request, res: Response) => {
+
+        const {
+            email: adminMailId,
+            password: adminPassword
+        } = req.body;
+    }
+);
+
+const residentLogin = catchAsync(
+    async (req: Request, res: Response) => {
+
+    }
+);
+
+const residentRegister = catchAsync(
+    async (req: Request, res: Response) => {
+
+    }
+);
+
+export {
+    adminLogin,
+    residentLogin,
+    residentRegister
+}
